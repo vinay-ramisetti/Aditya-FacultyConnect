18fc90a47b4fa6728d1994c2e7b9e01dd38917de
user.name like: "Vinay Ramisetti <user.email like: "vinayramisetti2004@gmail.com>
2025-03-09 17:49:19 +0530

    Fixed import issue

--- frontend/src/components/Teacher.jsx
+++ frontend/src/components/Teacher.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from './Navbar';
-import DisplayCourses from './DisplayCourses';
+import DisplayCourses from "./DisplayCourses.jsx";
 import Profile from './Profile';
 import DisplayFeedback from './DisplayFeedback';
 import ProctoringTable from './DisplayProctoring';
